c9f88aada7149c58144fd6328196001f706d48d9
Mingar3x <127351206+Mingar3x@users.noreply.github.com>
2024-08-23 16:21:05 +0000

    anomg us

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,26 @@
+public class BodyOfWater {
+    private String name;
+    private int widestDiameter;
+    private int avgDepth;
+    private boolean isSalt;
 
+    public BodyOfWater(String n, int d, int a, boolean s) {
+        name = n;
+        widestDiameter = d;
+        avgDepth = a;
+        isSalt = s;
+    }
+    public void printState() {
+        System.out.println("name: "+ name);
+        System.out.println("Widest Diameter: "+ widestDiameter);
+        System.out.println("Average Depth: "+ avgDepth);
+        System.out.println("Salty: "+ isSalt);
+    }
+    public String name() {
+        return name;
+    }
+    public void setName(String n) {
+        name = n;
+    }
+
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,14 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("small lake", 3, 3, false);
+        b.printState();
+        System.out.println(b.name());
+        b.setName("small lake1");
+        System.out.println(b.name());
+        BodyOfWater b1 = new BodyOfWater("small lake2", -3, -3, true);
+        b1.printState();
+        System.out.println(b1.name());
+        b1.setName("small lake3");
+        System.out.println(b1.name());
     }
 }
